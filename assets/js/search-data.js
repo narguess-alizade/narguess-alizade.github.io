88d4de7e824365c8c0e3fa2eb653e4a3629c6209
narguess-alizade <narguess.alizade@gmail.com>
2026-09-18 22:37:38 +0000

    Deploying to gh-pages from @ narguess-alizade/narguess-alizade.github.io@85e5451a98f0b495937904389068cb90a9dea8ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
+  },{id: "nav-bookshelf",
+          title: "Bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/bookshelf/";
+          },
+        },{id: "nav-research",
           title: "Research",
           description: "A growing collection of your cool projects.",
           section: "Navigation",

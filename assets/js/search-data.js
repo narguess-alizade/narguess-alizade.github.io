772ae4cd687ba0cb9a92db68e568716540e277b5
narguess-alizade <narguess.alizade@gmail.com>
2026-09-19 01:17:19 +0000

    Deploying to gh-pages from @ narguess-alizade/narguess-alizade.github.io@707ec48bb7217a1c58e527584e4f63cee27dfcd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "Courses and teaching experience.",
@@ -36,6 +29,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/outreach/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "nav-bookshelf",
           title: "Bookshelf",
